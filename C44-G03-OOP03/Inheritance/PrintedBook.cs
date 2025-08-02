d7cd2655e9073a1605460c83de57effc1daf1c5f
Mahmoud Mansour <mansour@gmail.com>
2025-08-02 22:25:15 +0300

    Complete Q05

---
+++ C44-G03-OOP03/Inheritance/PrintedBook.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C44_G03_OOP03.Inheritance
+{
+    internal class PrintedBook : Book
+    {
+        #region Properties
+        public int PageCount { get; set; }
+        #endregion
+
+        #region Constructors
+        public PrintedBook(string title, string author, string isbn, int pageCount)
+        : base(title, author, isbn)
+        {
+            PageCount = pageCount;
+        }
+        #endregion
+
+        #region Methods
+        public override void DisplayInfo()
+        {
+            base.DisplayInfo();
+            Console.WriteLine($"Page Count: {PageCount} pages");
+        }
+        #endregion
+    }
+}
--- C44-G03-OOP03/Program.cs
+++ C44-G03-OOP03/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace C44_G03_OOP03
+﻿using C44_G03_OOP03.Inheritance;
+
+namespace C44_G03_OOP03
 {
     internal class Program
     {
@@ -12,36 +14,45 @@
             #endregion
 
             #region Question 2 & 3 & 4
-            Employee[] EmpArr = new Employee[3];
+            //Employee[] EmpArr = new Employee[3];
 
-            EmpArr[0] = new Employee(1, "Ali", SecurityLevel.DBA, 15000m, new HireDate(12, 2, 2019), 'M');
-            EmpArr[1] = new Employee(2, "Sara", SecurityLevel.Guest, 5000m, new HireDate(5, 7, 2021), 'F');
-            EmpArr[2] = new Employee(3, "Omar", SecurityLevel.DBA, 18000m, new HireDate(25, 12, 2018), 'M');
+            //EmpArr[0] = new Employee(1, "Ali", SecurityLevel.DBA, 15000m, new HireDate(12, 2, 2019), 'M');
+            //EmpArr[1] = new Employee(2, "Sara", SecurityLevel.Guest, 5000m, new HireDate(5, 7, 2021), 'F');
+            //EmpArr[2] = new Employee(3, "Omar", SecurityLevel.DBA, 18000m, new HireDate(25, 12, 2018), 'M');
 
-            Console.WriteLine("Original Array:");
-            foreach (var emp in EmpArr)
-                Console.WriteLine(emp);
+            //Console.WriteLine("Original Array:");
+            //foreach (var emp in EmpArr)
+            //    Console.WriteLine(emp);
 
-            for (int i = 0; i < EmpArr.Length - 1; i++)
-            {
-                for (int j = i + 1; j < EmpArr.Length; j++)
-                {
-                    if (EmpArr[i].HireDate.ToDateTime() > EmpArr[j].HireDate.ToDateTime())
-                    {
-                        Employee temp = EmpArr[i];
-                        EmpArr[i] = EmpArr[j];
-                        EmpArr[j] = temp;
-                    }
-                }
-            }
+            //for (int i = 0; i < EmpArr.Length - 1; i++)
+            //{
+            //    for (int j = i + 1; j < EmpArr.Length; j++)
+            //    {
+            //        if (EmpArr[i].HireDate.ToDateTime() > EmpArr[j].HireDate.ToDateTime())
+            //        {
+            //            Employee temp = EmpArr[i];
+            //            EmpArr[i] = EmpArr[j];
+            //            EmpArr[j] = temp;
+            //        }
+            //    }
+            //}
 
-            Console.WriteLine("\nSorted Array by Hire Date:");
-            foreach (var emp in EmpArr)
-                Console.WriteLine(emp);
+            //Console.WriteLine("\nSorted Array by Hire Date:");
+            //foreach (var emp in EmpArr)
+            //    Console.WriteLine(emp);
 
-            Console.WriteLine("\nNumber of Boxing/Unboxing operations: 0");
+            #endregion
 
+            #region 5-Design a program for a library management system where:
+            EBook eBook = new EBook("Requiem for a Dream: A Novel", "Selby Jr., Hubert", "9781560252481", 446);
 
+            PrintedBook printedBook = new PrintedBook("Dark Matter: A Novel", "Crouch, Blake", "9781101904237", 325);
+
+            Console.WriteLine("EBook Info:");
+            eBook.DisplayInfo();
+
+            Console.WriteLine("\nPrinted Book Info:");
+            printedBook.DisplayInfo();
             #endregion
         }
     }
